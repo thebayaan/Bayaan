d198d370ccaa36112cfe587dfbb6ceb267b5ed52
OKKHALIL3 <omarkkhalil@gmail.com>
2026-03-02 19:10:45 -0800

    upgraded expo to 55 and other libraries

--- metro.config.js
+++ metro.config.js
@@ -2,16 +2,8 @@ const {getDefaultConfig} = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.transformer = {
-  ...config.transformer,
-  minifierConfig: {
-    ...config.transformer?.minifierConfig,
-  },
-};
-
 config.transformer.getTransformOptions = async () => ({
   transform: {
-    experimentalImportSupport: false,
     inlineRequires: true,
   },
 });
